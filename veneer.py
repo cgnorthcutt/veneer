57db56518fc6dba4f191395b3a0e572f82701ad8
Curtis Northcutt <cgn@richmond.mit.edu>
2019-05-13 17:10:45 -0400

    Creates veneer.txt if it does not exist. use - not *

--- veneer.py
+++ veneer.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import datetime
 import random
+import os
 
 
-# In[2]:
+# In[ ]:
 
 
+fn = 'veneer.txt'
 sunday_write_frac = 0.75
 
 months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
 months = dict(zip(range(1, 1 + len(months)), months))
 
 t = datetime.datetime.today()
-y = t.year
-m = t.month
-d = t.day
-w = t.weekday() # Saturday is 5, Sunday is 6
 
 
-# In[3]:
+# In[ ]:
 
 
-with open('README.md', 'a') as wf:
+if not os.path.isfile(fn):
+    # Set-up file if it does not exist.
+    with open(fn, 'w') as wf:
+        print("{}".format(t.year), file = wf)
+        print("{} ".format(months[t.month]), end = "", file = wf)
+        for i in range(1, 1 + t.day):
+            print("|" if i % 5 == 0 else "_", end = "", file = wf)
+else:
+    # Append to file if it does exist
+    with open(fn, 'a') as wf:
     if t.month == 1 and t.day == 1:
         print("\n{}".format(t.year), end = "", file = wf)
     if t.day == 1:
         print("\n{} ".format(months[t.month]), end = "", file = wf)
-    if t.day != 6 or random.random() < sunday_write_frac: # Only write on Sundays some fraction of the time.
-        print("|" if t.day == 5 else "*", end = "", file = wf)
+    if t.weekday() != 6 or random.random() < sunday_write_frac: # Only write on Sundays some fraction of the time.
+        print("|" if t.day == 5 else "-", end = "", file = wf)
 
