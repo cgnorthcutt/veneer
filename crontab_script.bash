8b9c32b75e3d8be2c90b3d75d3418333f5c7d1f1
Curtis Northcutt <cgn@pikeville.mit.edu>
2019-05-14 12:01:07 -0400

    Ready for production.

--- crontab_script.bash
+++ crontab_script.bash
@@ -1,4 +1,6 @@
-./veneer.py
-/usr/bin/git pull
-/usr/bin/git commit veneer.txt -m "Auto daily commit."
-/usr/bin/git push
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd "$SCRIPTPATH"
+veneer.py
+git pull
+git commit veneer.txt -m "Auto daily commit."
+git push
